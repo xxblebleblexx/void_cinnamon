3ed1bd4c80a1b5959004e194978fd77ffa4aa488
blebleble save ur day <108147789+xxblebleblexx@users.noreply.github.com>
2026-09-02 17:08:07 +0700

    Update WM dependencies and enable elogind service
    Replaced 'dbus' with 'elogind' in the installation of WM dependencies and added a symlink for 'elogind' service.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo xbps-install -u xbps
 
 #install WM depencies
-sudo xbps-install -S xorg dbus lightdm mesa intel-video-accel linux7.2 linux7.2-headers sassc patch mesa-vulkan-intel
+sudo xbps-install -S xorg elogind dbus lightdm mesa linux7.2 linux7.2-headers sassc patch
 
 #install cinnamon DE and cinnamon application 
 sudo xbps-install -S cinnamon nemo gnome-screenshot gnome-terminal vim gedit git wget papirus-icon-theme firefox power-profiles-daemon pfetch cronie rsv engrampa unzip
@@ -28,6 +28,7 @@ sudo rm -rf /var/service/dhcpcd
 sudo rm -rf /var/service/wpa_supplicant
 
 #enable services
+sudo ln -s /etc/sv/elogind /var/service/
 sudo ln -s /etc/sv/dbus /var/service/
 sudo ln -s /etc/sv/lightdm /var/service/
 sudo ln -s /etc/sv/NetworkManager /var/service/
